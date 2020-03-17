c3cf832d6b8b7baaaeb36667a455344a87307f71
Kinuk97 <kiukangg@gmail.com>
2020-03-17 17:55:38 +0900

    scanPackages 수정

--- src/main/java/com/practice/PracticeApplication.java
+++ src/main/java/com/practice/PracticeApplication.java
@@ -3,7 +3,7 @@ package com.practice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.practice.**"})
+@SpringBootApplication
 public class PracticeApplication {
 
 	public static void main(String[] args) {
