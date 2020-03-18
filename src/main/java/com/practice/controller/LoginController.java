88b29795bd1105e58c5f14029354e1d15252509c
Kinuk97 <kiukangg@gmail.com>
2020-03-18 21:43:33 +0900

    로그인 구현
    로그인, 아이디 저장 체크시 쿠키로 저장

--- src/main/java/com/practice/controller/LoginController.java
+++ src/main/java/com/practice/controller/LoginController.java
@@ -2,9 +2,19 @@ package com.practice.controller;
 
 import com.practice.dto.Member;
 import com.practice.service.LoginService;
+import com.sun.deploy.net.HttpResponse;
+import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class LoginController {
@@ -19,14 +29,38 @@ public class LoginController {
     public void moveLoginPage() {
     }
 
+    @ResponseBody
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String loginAction(Member member) {
-        System.out.println(member);
+    public Map<String, Object> loginAction(Member member, boolean saveEmail, HttpServletResponse response) {
+        Map<String, Object> map = new HashMap<>();
 
-        System.out.println(loginService.checkId(member));
+        // 로그인 성공시 회원정보
+        Member result = loginService.checkId(member);
 
-        System.out.println(member);
+        if (result != null) {
+            // 로그인 성공 결과
+            map.put("result", "1");
 
-        return "redirect:/";
+            // 아이디 저장 체크하면 쿠키에 이메일 저장
+            if (saveEmail) {
+                Cookie emailCookie = new Cookie("email", member.getEmail());
+
+                emailCookie.setMaxAge(7 * 24 * 60 * 60);
+
+                response.addCookie(emailCookie);
+            } else {
+                Cookie emailCookie = new Cookie("email", null);
+
+                emailCookie.setMaxAge(0);
+
+                response.addCookie(emailCookie);
+            }
+        } else {
+            // 로그인 실패 결과
+            map.put("result", "-1");
+            map.put("msg", "아이디 혹은 비밀번호를 확인해주세요.");
+        }
+
+        return map;
     }
 }
